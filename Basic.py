6ae12d478cd30ccdd9e4fbf1d6b8c5c44e637dd5
Harshita <starlastyle103@gmail.com>
2024-03-11 12:24:23 +0530

    Basic Api

--- Basic.py
+++ Basic.py
@@ -1,6 +1,3 @@
-# FastAPI is a tool that helps developers create web applications with Python really quickly.
-#  It's especially good at handling lots of requests at the same time, so it's fast. 
-#  It's also easy to use because it gives you helpful tools to make sure your code works properly and documents it automatically.
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -8,7 +5,7 @@ from typing import Optional
 
 app=FastAPI()
 
-# #our frontend is sending the exact data that we expect
+# to ensure that our frontend is sending the exact data that we expect
 # class Post(BaseModel):
 #     title:str
 #     content : str
@@ -16,18 +13,22 @@ app=FastAPI()
 #     rating:Optional[int]=None
 
 @app.get("/")   #act like an api--turns the code in path operation
-async def root():       #its function, async is only needed if u r performing async task--things like talking to db 
-    return {"message":"Hello World"}  #we r sending dic, which fast api will covert to json which is the main universal langusge of api
+async def root():       #its like a simple function, async is only needed if u r performing async task--eg like talking to db 
+    return {"message":"Hello World"}  #we r sending dictionary, which fast api will covert to json which is the main universal language of api
 
 
 @app.get("/posts")
 def get_posts():
     return {"data":"This is your post"}
 
-#noramlly we send https get request and our api give us all the data that we tryna get
-#but wth the post request we can send data to the api server. so posst request is used to creating things
+
+
+
+#Please give title and content in postman Api body
+#noramlly we send https get request and our api give us all the data that we trying to fetch.
+#but wth the post request we can send data to the api server and will get the respond. so post request is used to creating things
 @app.post("/createposts")
-def create_post(payload:dict= Body(...)):  #Body(..) days that body must have values
+def create_post(payload:dict= Body(...)):  #Body(..) says that body must have values
     print(payload)
     # return {"message":"successfully created post"}
     return {"new_post":f"title {payload['title']} content: {payload['content']}"} 
@@ -38,7 +39,6 @@ def create_post(payload:dict= Body(...)):  #Body(..) days that body must have va
 #     print(post.model_dump)
 #     # return {"message":"successfully created post"}
 #     return {"new_post":f"title {post.title} content: {post.content}"}  
-
 
 
 
